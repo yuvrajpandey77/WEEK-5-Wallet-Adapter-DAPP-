23fded868781b5a693db6b4f2e6098f85cc53bb8
Yuvraj Pandey <email@gmail.com>
2025-08-29 07:09:34 +0000

    fixed error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {  useMemo, type FC } from 'react';
+import {  type FC } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import {
     WalletModalProvider,
@@ -7,15 +7,10 @@ import { Wallet } from './Airdrop';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { clusterApiUrl } from '@solana/web3.js';
 
-const network = WalletAdapterNetwork.Devnet;
-// You can also provide a custom RPC endpoint
-const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 const App: FC = () => {     
     return (
-        <ConnectionProvider endpoint={endpoint}>
+        <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/hl5Gau0XVV37m-RDdhcRzqCh7ISwmOAe"}>
             <WalletProvider wallets={[]} autoConnect>
                 <WalletModalProvider>
                     <Wallet />
